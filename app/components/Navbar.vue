137c965ba5cca1ea69ddf7897a231e0720299f55
DanielPreil <preildaniel15@gmail.com>
2025-10-14 12:55:58 +0200

    style: Navbar (logo + navLinks), minimal animations, no responsiveness

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -1,10 +1,21 @@
 <template>
-  <header class="bg-orange-500">
-    <nav>
-      <NuxtLink to="/">Project Name</NuxtLink>
-      <div class="navLinks">
-        <NuxtLink to="/">Főoldal</NuxtLink>
-        <NuxtLink to="/about">About</NuxtLink>
+  <header class="fixed top-0 left-0 w-full">
+    <nav class="px-32 py-3 flex items-center justify-between w-full">
+      <NuxtLink to="/" class="flex items-center gap-2">
+        <img class="noSelect w-[4.7rem]" src="/logo.png" alt="Logo" />
+        <span class="noSelect font-caesar uppercase font-bold text-3xl" to="/">Pantherium</span>
+      </NuxtLink>
+      <div class="flex items-center p-2 w-fit gap-4 font-bold">
+        <NuxtLink class="hover:text-accent duration-300" to="/">Home</NuxtLink>
+        <NuxtLink class="hover:text-accent duration-300" to="/about">About</NuxtLink>
+        <NuxtLink class="hover:text-accent duration-300" to="/">Quiz</NuxtLink>
+        <NuxtLink class="hover:text-accent duration-300" to="/">Animals | Plants</NuxtLink>
+        <NuxtLink
+          class="hover:bg-accent duration-300 uppercase font-caesar text-xl rounded bg-primary text-[#09100b] px-2 py-1"
+          to="/"
+        >
+          Intrested
+        </NuxtLink>
       </div>
     </nav>
   </header>
