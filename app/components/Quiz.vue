1f1acac7c893a8fa2924bba917eb6155c8d25b4e
Mate720 <90619512+Mate720@users.noreply.github.com>
2025-10-14 13:47:45 +0200

    feat: Component communication

---
+++ app/components/Quiz.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const titles = ["Animals","Plants", "Everything"]
+</script>
+
+<template>
+    <h1 class="text-center text-6xl m-2">Quizezz</h1>
+  <div class="bg-amber-500 flex justify-around">
+    <div v-for="(card, index) in 3">
+      <QuizCard :text="titles[index]"  />
+    </div>
+  </div>
+</template>
