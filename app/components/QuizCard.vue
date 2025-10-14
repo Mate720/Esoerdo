590e26f994ae301feb8318044c26713845ee521b
Mate720 <90619512+Mate720@users.noreply.github.com>
2025-10-14 13:59:40 +0200

    feat:  add props to QuizCard

--- app/components/QuizCard.vue
+++ app/components/QuizCard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const props = defineProps<{
-  text: string|undefined
+  title: string|undefined
+  description: string|undefined
 }>()
 </script>
 
@@ -8,10 +9,10 @@ const props = defineProps<{
   <div class="p-5 bg-sky-500 rounded-xl">
     <NuxtImg src="/blankimg.png" />
     <div>
-      <h2>{{text}}</h2>
+      <h2>{{title}}</h2>
     </div>
     <div>
-      <p>Question only about animals</p>
+      <p>{{description}}</p>
     </div>
     <div class="pt-5"><Button label="Test me!" /></div>
   </div>
