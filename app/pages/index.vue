25a3fea2d258caaffb5332ec4f9bf0a2b7d4959b
DanielPreil <preildaniel15@gmail.com>
2025-10-14 12:54:19 +0200

    feat: use RotatingText component in hero

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,20 @@
 <template>
   <section>
     <h1>Főoldal</h1>
-    <p>Üdvözöllek a Főoldalon.</p>
+    <div class="flex text-5xl">
+      <span class="font-bold bg-[#eff6f1] text-[#0d120e] px-4 py-1 rounded-l w-fit">Explore</span>
+      <RotatingText
+        :texts="['Wildlife', 'Beasts', 'Nature', 'Ecosystem']"
+        mainClassName="bg-primary text-black px-4 py-1 font-caesar rounded-r w-fit"
+        staggerFrom="last"
+        :initial="{ y: '100%' }"
+        :animate="{ y: 0 }"
+        :exit="{ y: '-120%' }"
+        :staggerDuration="0.025"
+        splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+        :transition="{ type: 'spring', damping: 30, stiffness: 400 }"
+        :rotationInterval="3000"
+      />
+    </div>
   </section>
 </template>
