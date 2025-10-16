81bb2d5ee84cc4efe285bf9d3b87cd6b62471a29
DanielPreil <preildaniel15@gmail.com>
2025-10-16 11:24:10 +0200

    fix: better type handling

--- app/components/QuizCard.vue
+++ app/components/QuizCard.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
 const props = defineProps<{
-  title: string | undefined;
-  description: string | undefined;
+  title: string;
+  description: string;
+  imgSource?: string;
 }>();
 </script>
 
 <template>
-  <div class="p-5 bg-sky-500 rounded-xl">
-    <NuxtImg src="/blankimg.png" />
+  <article class="p-5 bg-sky-500 rounded-xl">
+    <!-- keep case consistent with file on disk -->
+    <NuxtImg :src="imgSource ?? '/blankImg.png'" alt="card image" />
     <div>
       <h2>{{ title }}</h2>
     </div>
     <div>
       <p>{{ description }}</p>
     </div>
-    <div class="pt-5">
-      <Button label="Test me!" />
-    </div>
-  </div>
+    <div class="pt-5"><Button label="Test me!" /></div>
+  </article>
 </template>
