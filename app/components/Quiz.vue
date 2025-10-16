a267a6d83b052600570ef61e395eb47c29e10284
DanielPreil <preildaniel15@gmail.com>
2025-10-16 11:25:11 +0200

    fix: Quiz categories, and rendering

--- app/components/Quiz.vue
+++ app/components/Quiz.vue
@@ -1,34 +1,38 @@
 <script setup lang="ts">
 const categories = [
   {
-    key: 1,
+    id: 1,
     title: "Animals",
     link: "/",
     description: "Questions only about animals",
-    imgSource: "/blankImg.png"
+    imgSource: "/blankImg.png",
   },
   {
-    key: 2,
+    id: 2,
     title: "Plants",
     link: "/plants",
     description: "Questions only about plants",
-    imgSource: "/blankImg.png"
+    imgSource: "/blankImg.png",
   },
   {
-    key: 3,
+    id: 3,
     title: "Everything",
     link: "/everything",
     description: "Questions about animals and plants",
-    imgSource: "/blankImg.png"
-  }
+    imgSource: "/blankImg.png",
+  },
 ];
 </script>
 
 <template>
-  <h1 class="text-center text-6xl m-2">Quizezz</h1>
-  <div class="bg-amber-500 flex justify-around">
-    <div v-for="(card, index) in categories">
-      <QuizCard :title="categories[index]!.title" :description="categories[index]!.description" :key="categories[index]!.key" />
+  <div class="quiz-list">
+    <h1 class="text-center text-6xl m-2">Quizezz</h1>
+
+    <div class="bg-amber-500 flex justify-around">
+      <!-- :key must be on the element created by v-for -->
+      <div v-for="card in categories" :key="card.id" class="mx-2">
+        <QuizCard :title="card.title" :description="card.description" :img-source="card.imgSource" />
+      </div>
     </div>
   </div>
 </template>
