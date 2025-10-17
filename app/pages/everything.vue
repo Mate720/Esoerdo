1e527bc2a7a79ed395de33fd20d4efc208113632
Mate720 <90619512+Mate720@users.noreply.github.com>
2025-10-17 19:45:49 +0200

    rafactor: import composable

--- app/pages/everything.vue
+++ app/pages/everything.vue
@@ -1,12 +1,51 @@
+<script setup lang="ts">
+import { useEveryQuestions } from '~/composable/useEveryQuestions';
+const { questions, next, correct, actual, checkAnswer, onSelect, reset, resultMessage } = useEveryQuestions();
+</script>
+
 <template>
   <section>
+     <Toast position="bottom-right" group="br" />
     <h1>Quiz</h1>
     <p>Ez az everything quiz oldal.</p>
   </section>
-  <section>
-    <h1>IDE FOGNKA KERÜLNI A KÉRDÉSEK</h1>
-      <div class="flex justify-center">
-        <NuxtLink class="bg-accent p-2 text-xl rounded-md" to="/quiz">Go back!</NuxtLink>
-        </div>
+  <section v-if="next < 10">
+    <h1>Score: 10 / {{ correct }}</h1>
+    <h3>{{ questions[actual]?.question }}</h3>
+    <div>
+      <div>
+        <RadioButtonGroup class="flex flex-col">
+          <div v-for="(choice, index) in questions[actual]?.answers">
+            <RadioButton :value="choice" @change="() => onSelect(index)" />
+            <label>{{ choice }}</label>
+          </div>
+        </RadioButtonGroup>
+      </div>
+      <button class="bg-accent p-2 text-xl rounded-md cursor-pointer" type="button" @click="checkAnswer()">Check
+        answer</button>
+    </div>
+  </section>
+
+  <section v-else>
+    <p>End of Quiz</p>
+    <p>Results: 10 / {{ correct }}</p>
+    <p>{{ resultMessage }}</p>
+    <div class="flex justify-center">
+      <NuxtLink class="bg-accent p-2 text-xl rounded-md  cursor-pointer" to="/quiz" @click="reset()">Go back!
+      </NuxtLink>
+      <button class="bg-accent p-2 text-xl rounded-md  cursor-pointer" type="button" @click="reset()">Restart</button>
+    </div>
   </section>
 </template>
+
+
+<style>
+button {
+  transition: background-color 0.5s ease, color 0.5s ease;
+}
+
+button:hover {
+  background-color: #fff;
+  color: #32c35e;
+}
+</style>
