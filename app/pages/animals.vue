bdf9124d8c66955ace59013da6ae89239423ee04
Mate720 <90619512+Mate720@users.noreply.github.com>
2025-10-17 19:13:19 +0200

    refactor: import composables

--- app/pages/animals.vue
+++ app/pages/animals.vue
@@ -1,92 +1,23 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-import { ref, reactive } from 'vue';
-import RadioButton from 'primevue/radiobutton';
-import RadioButtonGroup from 'primevue/radiobuttongroup';
-import AnimalQuestions from '~/data/AnimalQuestion';
-import Toast from 'primevue/toast';
+import { useAnimalQuestions } from '~/composable/useAnimalQuestions';
 
-const route = useRoute();
-
-
-
-const actual = ref(0);
-const correct = ref(0);
-const selectedIndex = ref(-1);
-const toast = useToast();
-
-
-const checkAnswer = () => {
-    console.log(selectedIndex.value);
-    if (selectedIndex.value === -1) {
-        toast.add({ severity: 'warn', summary: 'Warn Message', detail: 'Please choose an option!', group: 'br', life: 1500 });
-    }
-    else if (selectedIndex.value === AnimalQuestions[actual.value]?.correctIndex) {
-        correct.value++;
-        toast.add({ severity: 'success', summary: 'Correct!', group: 'br', life: 1500 });
-        actual.value++;
-        selectedIndex.value = -1;
-    } else {
-        toast.add({ severity: 'error', summary: 'Incorrect answer!', group: 'br', life: 1500 });
-        actual.value++;
-        selectedIndex.value = -1;
-    }
-}
-
-const onSelect = (index: number) => {
-    selectedIndex.value = index;
-    console.log(selectedIndex);
-}
-
-const reset = () => {
-    actual.value = correct.value = 0;
-    selectedIndex.value = -1;
-    console.log(actual.value);
-}
-
-watch(
-    () => route.fullPath,
-    () => {
-        actual.value = correct.value = 0;
-        selectedIndex.value = -1;
-    }
-)
-
-
-const messages: Record<number, string> = {
-    0: "Don't give up, keep practicing!",
-    1: "Keep trying, you'll get better!",
-    2: "Not bad, but there is room for improvement.",
-    3: "You did okay!",
-    4: "Good job, you're improving!",
-    5: "Well done! Keep it up!",
-    6: "Great work!",
-    7: "Awesome! Almost there!",
-    8: "Excellent!",
-    9: "Almost perfect!",
-    10: "Excellent! Perfect score!"
-};
-
-const resultMessage = computed(() => {
-    return messages[correct.value];
-});
+const { questions, next, correct, actual, checkAnswer, onSelect, reset, resultMessage } = useAnimalQuestions();
 
 </script>
 
 <template>
     <section>
         <Toast position="bottom-right" group="br" />
-        <h1>Quiz</h1>
-        <p>Ez az Animals quiz oldal.</p>
+        <h1>Animal Quiz</h1>
     </section>
-    <section v-if="actual < 10">
-        <h1>HELYES VÁLASZOK: 10 / {{ correct }}</h1>
-        <h3>{{ AnimalQuestions[actual]?.question }}</h3>
+    <section v-if="next < 10">
+        <h1>Score: 10 / {{ correct }}</h1>
+        <h3>{{ questions[actual]?.question }}</h3>
 
         <div>
             <div>
                 <RadioButtonGroup class="flex flex-col">
-                    <div v-for="(choice, index) in AnimalQuestions[actual]?.answers">
+                    <div v-for="(choice, index) in questions[actual]?.answers">
                         <RadioButton :value="choice" @change="() => onSelect(index)" />
                         <label>{{ choice }}</label>
                     </div>
@@ -116,3 +47,15 @@ const resultMessage = computed(() => {
 
 
 </template>
+
+
+<style>
+button {
+    transition: background-color 0.5s ease, color 0.5s ease;
+}
+
+button:hover {
+    background-color: #fff;
+    color: #32c35e;
+}
+</style>
--- app/pages/plants.vue
+++ app/pages/plants.vue
@@ -1,12 +1,61 @@
+<script setup lang="ts">
+import { usePlantQuestions } from '~/composable/usePlantQuestions';
+
+const { questions, next, correct, actual, checkAnswer, onSelect, reset, resultMessage } = usePlantQuestions();
+
+
+</script>
+
 <template>
   <section>
-    <h1>Quiz</h1>
-    <p>Ez az Pl     ants quiz oldal.</p>
+    <Toast position="bottom-right" group="br" />
+    <h1>Plants Quiz</h1>
   </section>
-  <section>
-    <h1>IDE FOGNKA KERÜLNI A KÉRDÉSEK</h1>
-       <div class="flex justify-center">
-        <NuxtLink class="bg-accent p-2 text-xl rounded-md" to="/quiz">Go back!</NuxtLink>
-        </div>
+  <section v-if="next < 10">
+    <h1>Score: 10 / {{ correct }}</h1>
+    <h3>{{ questions[actual]?.question }}</h3>
+
+    <div>
+      <div>
+        <RadioButtonGroup class="flex flex-col">
+          <div v-for="(choice, index) in questions[actual]?.answers">
+            <RadioButton :value="choice" @change="() => onSelect(index)" />
+            <label>{{ choice }}</label>
+          </div>
+        </RadioButtonGroup>
+      </div>
+      <button class="bg-accent p-2 text-xl rounded-md cursor-pointer" type="button" @click="checkAnswer()">Check
+        answer</button>
+
+    </div>
+
   </section>
+
+  <section v-else>
+    <p>End of Quiz</p>
+    <p>Results: 10 / {{ correct }}</p>
+    <p>{{ resultMessage }}</p>
+
+    <div class="flex justify-center">
+      <NuxtLink class="bg-accent p-2 text-xl rounded-md  cursor-pointer" to="/quiz" @click="reset()">Go back!
+      </NuxtLink>
+      <button class="bg-accent p-2 text-xl rounded-md  cursor-pointer" type="button" @click="reset()">Restart</button>
+
+    </div>
+  </section>
+
+
+
 </template>
+
+
+<style>
+button {
+  transition: background-color 0.5s ease, color 0.5s ease;
+}
+
+button:hover {
+  background-color: #fff;
+  color: #32c35e;
+}
+</style>
