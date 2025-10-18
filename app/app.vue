2f21b7641ca57ae83b6b3bbdc08f0dd2993fc7a6
Daniel Preil <90137943+DanielPreil@users.noreply.github.com>
2025-10-18 23:07:01 +0200

    style: add responsivity to sections by changing different gap sizes

--- app/app.vue
+++ app/app.vue
@@ -1,13 +1,18 @@
 <script setup>
 useHead({
-  bodyAttrs: { class: "dark pt-32" }
+  bodyAttrs: { class: "dark pt-[8rem]" }
 });
 </script>
 
 <template>
   <Navbar />
-  <main class="px-6 md:px-12 lg:px-32">
+  <main
+    class="px-6 md:px-12 lg:px-32 flex flex-col gap-[5rem] sm:gap-[7rem] md:gap-[9rem] xl:gap-[12rem] lg:gap-[15rem]">
     <NuxtPage />
   </main>
-  <Footer />
+  <footer class="w-full bg-blue-500">
+    <div>
+      <p>Minden jog fenntartva</p>
+    </div>
+  </footer>
 </template>
