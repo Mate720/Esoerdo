8fbee5a940ba6c1e9c5884bc2c0c4ea94c51e57d
Daniel Preil <90137943+DanielPreil@users.noreply.github.com>
2025-10-20 21:55:44 +0200

    refactor: replace footer with Footer component

--- app/app.vue
+++ app/app.vue
@@ -10,9 +10,5 @@ useHead({
     class="px-6 md:px-12 lg:px-32 flex flex-col gap-[5rem] sm:gap-[7rem] md:gap-[9rem] xl:gap-[12rem] lg:gap-[15rem]">
     <NuxtPage />
   </main>
-  <footer class="w-full bg-blue-500">
-    <div>
-      <p>Minden jog fenntartva</p>
-    </div>
-  </footer>
+  <Footer />
 </template>
