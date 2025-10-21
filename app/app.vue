3e7919e80ef27c0d42b783f3959ac7dd3894de69
Daniel Preil <90137943+DanielPreil@users.noreply.github.com>
2025-10-21 21:37:52 +0200

    style: refactor layout structure in app.vue for improved responsivitiy

--- app/app.vue
+++ app/app.vue
@@ -1,14 +1,15 @@
 <script setup>
 useHead({
-  bodyAttrs: { class: "dark pt-[8rem]" }
+  bodyAttrs: { class: "dark" }
 });
 </script>
 
 <template>
-  <Navbar />
-  <main
-    class="px-6 md:px-12 lg:px-32 flex flex-col gap-[5rem] sm:gap-[7rem] md:gap-[9rem] xl:gap-[12rem] lg:gap-[15rem]">
-    <NuxtPage />
-  </main>
-  <Footer />
+  <div class="flex flex-col min-h-screen pt-[8rem]">
+    <Navbar />
+    <main class="flex-1 px-6 md:px-12 lg:px-32">
+      <NuxtPage />
+    </main>
+    <Footer />
+  </div>
 </template>
