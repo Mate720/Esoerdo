d4b293f6d5d905703f54888557773ebce966a8cf
Mate720 <90619512+Mate720@users.noreply.github.com>
2025-10-21 22:20:19 +0200

    fix: disable devtools in nuxt configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import Aura from "@primeuix/themes/aura";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ["~/assets/css/globals.css"],
   vite: {
     plugins: [tailwindcss()]
